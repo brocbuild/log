cf0b83442899dab2a0cc30209b8f536ece971183
liruihao <liruihao@baidu.com>
2015-12-16 13:16:55 +0800

    add sources files

---
+++ log.cpp
@@ -0,0 +1,47 @@
+/***************************************************************************
+ * 
+ * Copyright (c) 2015 Baidu.com, Inc. All Rights Reserved
+ * 
+ **************************************************************************/
+ 
+ 
+ 
+/**
+ * @file log.cpp
+ * @author liruihao(com@baidu.com)
+ * @date 2015/12/16 11:48:45
+ * @brief 
+ *  
+ **/
+
+#include "brocbuild/log/log.h"
+
+log::log()
+{
+}
+
+log::~log()
+{
+}
+
+void log::set_log_file(std::string filename)
+{
+    log_file = filename;
+}
+
+std::string log::get_log_file()
+{
+    return log_file;
+}
+
+void log::set_log_dir(std::string dirname)
+{
+    log_dir = dirname;
+}
+
+std::string log::get_log_dir()
+{
+    return log_dir;
+}
+
+/* vim: set expandtab ts=4 sw=4 sts=4 tw=100: */
---
+++ log.h
@@ -0,0 +1,40 @@
+/***************************************************************************
+ * 
+ * Copyright (c) 2015 Baidu.com, Inc. All Rights Reserved
+ * 
+ **************************************************************************/
+ 
+ 
+ 
+/**
+ * @file log.h
+ * @author liruihao(com@baidu.com)
+ * @date 2015/12/16 11:46:12
+ * @brief 
+ *  
+ **/
+
+
+
+
+#ifndef  BROCBUILD_LOG_LOG_H
+#define  BROCBUILD_LOG_LOG_H
+
+#include <string>
+
+class log {
+    public:
+        log();
+        ~log();
+        void set_log_file(std::string filename);
+        std::string get_log_file();
+        void set_log_dir(std::string dirname);
+        std::string get_log_dir();
+    private:
+        std::string log_file;
+        std::string log_dir;
+};
+
+#endif  //__LOG_H_
+
+/* vim: set expandtab ts=4 sw=4 sts=4 tw=100: */
